2e8c29608b66c23da615932ed143ab2cf1ccf95b
Steven La <stevenla@g.ucla.edu>
2018-10-12 18:56:53 -0700

    add first button component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 
+import Button from "../components/button";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Welcome to Color</h1>
+        <Button>this is a button</Button>
       </div>
     );
   }
