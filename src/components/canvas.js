43727ec57654050aa01021a89497c4e3c039694a
Steven La <stevenla@g.ucla.edu>
2018-11-02 18:28:50 -0700

    removed undo func. for now

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import P5Wrapper from "react-p5-wrapper";
 
 //array of drawings to push and splice
-var drawings = [];
+//var drawings = [];
 
 const sketch = p5 => {
   let curr_color;
-  let prevX;
-  let prevY;
-  // let md = false; //mouse dragged
+  //let prevX;
+  //let prevY;
+  //let md = false; //mouse dragged
 
   const color_options = {
     scheme_1: ["#94EBD8", "#00B349"],
@@ -23,9 +23,9 @@ const sketch = p5 => {
 
   p5.draw = () => {
     // do nothing
-    for (var i = 0; i < drawings.length; i++) {
-      drawings[i].update();
-    }
+    //for (var i = 0; i < drawings.length; i++) {
+    //  drawings[i].update();
+    //}
     // if (drawings.length){
     //   drawings[drawings.length-1].update();
     // }
@@ -34,21 +34,23 @@ const sketch = p5 => {
   p5.mousePressed = () => {
     // prevX = p5.pmouseX;
     // prevY = p5.pmouseY;
-  }
-
-  p5.mouseDragged = () => {
-    // p5.strokeWeight(10);
-    // p5.stroke(curr_color);
-    // p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
-    prevX = p5.pmouseX;
-    prevY = p5.pmouseY;
-    // drawings[drawings.length-1].update();
   };
 
+  p5.mouseDragged = () => {
+    p5.strokeWeight(10);
+    p5.stroke(curr_color);
+    p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
+    //prevX = p5.pmouseX;
+    //prevY = p5.pmouseY;
+    //drawings[drawings.length-1].update();
+  };
+
+  /*
   p5.mouseReleased = () => {
-    drawings.push(new Drawing(prevX, prevY, p5.mouseX, p5.mouseY));;
+    drawings.push(new Drawing(prevX, prevY, p5.mouseX, p5.mouseY));
     console.log(drawings);
-  }
+  };
+  */
 
   p5.keyPressed = () => {
     switch (p5.key) {
@@ -76,17 +78,17 @@ const sketch = p5 => {
         curr_color = p5.color(color_options.scheme_3[1]);
         break;
     }
-    
+    /*
     if (p5.key === "D" || p5.key === "d") {
       if (drawings.length > 0){
         drawings[drawings.length-1].undo();
         console.log(drawings);
       }
     }
-    
-
+    */
   };
 
+  /*
   function Drawing(x, y, px, py) {
     this.x = x;
     this.y = y;
@@ -103,16 +105,18 @@ const sketch = p5 => {
       p5.strokeWeight(10);
       p5.stroke(curr_color);
       // if (p5.mouseIsPressed === true) {
-        p5.line(this.x, this.y, this.px, this.py);
+      p5.line(this.x, this.y, this.px, this.py);
       // }
-    }
+    };
 
     this.undo = () => {
-      drawings.splice(drawings.length-1, 1);  //remove Drawing from array
+      drawings.splice(drawings.length - 1, 1); //remove Drawing from array
       p5.clear();
-    }
-  };
+    };
+  }
+  */
 
+  /*
   function Line(x, y, px, py) {
     this.x = x;
     this.y = y;
@@ -122,12 +126,11 @@ const sketch = p5 => {
     this.drawLine = () => {
       p5.strokeWeight(10);
       p5.stroke(curr_color);
-      p5.line(this.x, this.y, this.px, this.py)
-    }
+      p5.line(this.x, this.y, this.px, this.py);
+    };
   }
+  */
 };
-
-
 
 function Canvas(props) {
   return <P5Wrapper sketch={sketch} />;
