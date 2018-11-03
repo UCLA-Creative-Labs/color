b2f3f087632adc73942996e539b639c7005794d6
kristi <kristi5327@gmail.com>
2018-11-02 17:39:52 -0700

    adding modal code to comments

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './modal.css';
 
+//how to use: paste the stuff below into app.js under <Button>
+// use Ctrl / to block uncomment
+// { this.state.isShowing ? <div onMouseLeave={this.closeModalHandler} className="back-drop"></div> : null }
+// <button className="open-modal-btn" onMouseEnter={this.openModalHandler} onMouseLeave={this.closeModalHandler}>?</button>
+// <Modal
+//    className="modal"
+//    show={this.state.isShowing}
+//    close={this.closeModalHandler}>
+//        This is such a cool project whoo go draw shit and hear it make music amazing <br/>
+//        Brought to u by us
+// </Modal>
+
 const modal = (props) => {
     return (
         <div>
--- src/containers/app.js
+++ src/containers/app.js
@@ -30,7 +30,15 @@ closeModalHandler = () => {
       <div className="App">
         <h1>Welcome to Color</h1>
         <Button>this is a button</Button>
-
+        { this.state.isShowing ? <div onMouseLeave={this.closeModalHandler} className="back-drop"></div> : null }
+         <button className="open-modal-btn" onMouseEnter={this.openModalHandler} onMouseLeave={this.closeModalHandler}>?</button>
+         <Modal
+            className="modal"
+            show={this.state.isShowing}
+            close={this.closeModalHandler}>
+                This is such a cool project whoo go draw shit and hear it make music amazing <br/>
+                Brought to u by us
+        </Modal> 
         <Canvas />
       </div>
     );
