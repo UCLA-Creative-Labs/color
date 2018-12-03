c8eaaaf9b6d115018b04cfa4934f21a4c41e6af1
Steven La <stevenla@g.ucla.edu>
2018-12-02 18:59:29 -0800

    i can't believe this works lmao

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -3,7 +3,7 @@ import "./landing.css";
 
 // page layouts
 const landing_page_content = <h1>Color</h1>;
-let description_page_content = (
+const description_page_content = (
   <div>
     <h2>
       Turning colorful drawings into musical notes: a visuals music-alizer.
@@ -11,27 +11,70 @@ let description_page_content = (
     <h2>Made with ❤ from Creative Labs</h2>
   </div>
 );
+const how_to_page_content = (
+  <div>
+    <div id="how_to_div">
+      <h3 id="how_to">
+        <u>How To:</u>
+      </h3>
+      <img src={require("../../Images/scroll_down.png")} />
+    </div>
+    <div id="drag_to_draw_div">
+      <img src={require("../../Images/drag.png")} />
+      <h3>Drag to draw</h3>
+    </div>
+    <div id="press_div">
+      <h3>Keyboard Controls:</h3>
+      <div id="press_options">
+        <table>
+          <tr>D - Undo last stroke</tr>
+          <tr>Q - Redo last erased stroke</tr>
+          <tr>R - Reset canvas and color</tr>
+          <tr>T - Teal color</tr>
+          <tr>G - Green color</tr>
+          <tr>Y - Purple color</tr>
+          <tr>H - Orange color</tr>
+          <tr>U - Gold color</tr>
+          <tr>J - Blue color</tr>
+        </table>
+      </div>
+    </div>
+    <div id="next_button_div">
+      <button>Let's draw!</button>
+    </div>
+  </div>
+);
 
 class Landing extends Component {
   constructor() {
     super();
     this.state = {
-      order: 4
+      order: 1
     };
   }
 
   componentDidMount() {
     setTimeout(
       function() {
-        this.setState({ order: 4 });
+        this.setState({ order: 2 });
       }.bind(this),
       5000
+    );
+    setTimeout(
+      function() {
+        this.setState({ order: 3 });
+      }.bind(this),
+      15000
     );
   }
 
   render() {
     let content =
-      this.state.order === 1 ? landing_page_content : description_page_content;
+      this.state.order === 1
+        ? landing_page_content
+        : this.state.order === 2
+        ? description_page_content
+        : how_to_page_content;
     return (
       <div
         className={
@@ -40,40 +83,10 @@ class Landing extends Component {
             : "how_to_page_div"
         }
       >
-        <div id="how_to_div">
-          <h3 id="how_to">
-            <u>How To:</u>
-          </h3>
-          <img src={require("../../Images/scroll_down.png")} />
-        </div>
-        <div id="drag_to_draw_div">
-          <img src={require("../../Images/drag.png")} />
-          <h3>Drag to draw</h3>
-        </div>
-        <div id="press_div">
-          <h3>Keyboard Controls:</h3>
-          <div id="press_options">
-            <table>
-              <tr>D - Undo last stroke</tr>
-              <tr>Q - Redo last erased stroke</tr>
-              <tr>R - Reset canvas and color</tr>
-              <tr>T - Teal color</tr>
-              <tr>G - Green color</tr>
-              <tr>Y - Purple color</tr>
-              <tr>H - Orange color</tr>
-              <tr>U - Gold color</tr>
-              <tr>J - Blue color</tr>
-            </table>
-          </div>
-        </div>
-        <div id="next_button_div">
-          <button>Let's draw!</button>
-        </div>
+        {content}
       </div>
     );
   }
 }
 
 export default Landing;
-
-// {content}
